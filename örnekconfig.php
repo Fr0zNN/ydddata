c1eac4f8e542be93f197204a9efde4009b64d44a
Fr0zNN <68872170+Fr0zNN@users.noreply.github.com>
2022-11-16 00:23:55 +0300

    Add files via upload

---
+++ örnekconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+$DB_HOST = "DB HOST NAME";
+$DB_USER = "DB USER NAME";
+$DB_PASS = "";
+$DB_NAME = "DB NAME";
+
+$enablemail = true;
+
+$emailserverhost = "EMAİL HOST's Email"; #server.yddhosting.com VS. VS
+$emailserverid = "test@fsnhost.com";
+$emailserverpass = "DATA ŞİFRESİ BUNUDA ANLARSIN YİĞİT ABİ BENCE :D";
+
+$link = "https://localhost";
